81254798d4302d3529f136828fbd66f8b7f34c2e
binxxi <1551097460@qq.com>
2021-03-15 19:02:35 +0800

    my_ls

--- linux_study/lss.c
+++ linux_study/lss.c
@@ -11,26 +11,275 @@
 #include <pwd.h>
 #include <errno.h>
 
-#define PARAM_NONE 0
-#define RARAM_A 1
-#define PARAM_L 1
+#define _NONE 0
+#define _A 1
+#define _L 2
+#define MAX 1000
 #define MAXROWLEN 80
 
 int g_leave_len = MAXROWLEN;
 int g_maxlen;
-int files_num;
-int file_num[100];
+int _R = 0;
 
-void my_err(const char* err_string, int line)
+void my_err(const char* err_string, int line); 
+void display(int flag_param, char* filename);
+void display_attribute(struct stat buf, char * name, char* filename);
+void display_dir(int flag_param, char* path);
+void display_1(char* name, char* filename);
+void display_r(int flag_praram, char* filename);
+void quicksort(char **a, int left, int right);
+int getmid(int left,int right, char **a);
+int get_color(char* filename);
+void swap(char** a, int i, int j);
+
+int main(int argc, char** argv)
 {
-    fprintf(stderr, "line : %d ", line);
-    perror(err_string);
-    exit(1);
+    char path[MAX];
+    char param[32];
+    int flag_param = _NONE;
+    int i, k, j = 0, num = 0;
+    struct stat buf;
+
+    //
+    for(i = 1; i < argc; i++)
+    {
+        if(argv[i][0] == '-')
+        {
+            for( k = 1; k < strlen(argv[i]); k++, j++)
+            {
+                param[j] = argv[i][k];
+            }
+            num++;
+        }
+    }
+
+    for(i = 0; i < j; i++)
+    {
+        if(param[i] == 'a')
+        {
+            flag_param |= _A;
+            continue;
+        }else if(param[i] == 'l')
+        {
+            flag_param |= _L;
+            continue;
+        }else if(param[i] == 'R')
+        {
+            _R = 1;
+            continue;
+        }else
+        {
+            printf("my_ls:invaild option -%c\n", param[i]);
+            exit(1);
+        }
+    }
+
+
+    if(argc == (num+1))
+    {
+        strcpy(path,"./");
+        path[2] = '\0';
+        display_dir(flag_param, path);
+        return 0;
+    }
+
+    i = 1;
+    do{
+        if(argv[i][0] == '-') 
+        {
+            i++;
+            continue;
+        }
+        else
+        {
+            strcpy(path, argv[i]);
+            if(stat(path, &buf) == -1) my_err("stat", __LINE__);
+
+            if(S_ISDIR(buf.st_mode))
+            {
+                if(path[strlen(argv[i])-1] != '/')
+                {
+                    path[strlen(argv[i])] = '/';
+                    path[strlen(argv[i])+1] = '\0';
+                }
+                else path[strlen(argv[i])] = '\0';
+                display_dir(flag_param, path);
+                i++;
+            }
+            else
+            {
+                display(flag_param, path);
+                i++;
+            }
+        }
+    }while(i<argc);
+
+    return 0;
 }
 
-/*void display_attribute(struct stat buf, char * name)
+void display_r(int flag_param,char* filename)
 {
-    //char buf_time[32];
+    struct stat buf;
+    lstat(filename, &buf);
+
+    if(S_ISDIR(buf.st_mode))
+    {
+        int i, j = 0;
+        char name[MAX];
+        for(i = 0; i < strlen(filename); i++)
+        {
+            if(filename[i] == '/')
+            {
+                j = 0;
+                continue;
+            }
+            name[j++] = filename[i];
+        }
+        name[j] = '\0';
+        switch (flag_param){
+            case _NONE :
+                if(name[0] != '.') display_dir(flag_param,filename);
+                break;
+            case _A :
+                display_dir(flag_param, filename);
+                break;
+            case _L+_A :
+                display_dir(flag_param, filename);
+                break;
+            case _L :
+                if(name[0] != '.') display_dir(flag_param,filename);
+            default :
+                break;
+        } 
+    }
+}
+
+void display(int flag_param, char* filename)
+{
+    struct stat buf;
+    char name[MAX];
+    int i, j;
+
+    for(i = 0, j = 0; i < strlen(filename); i++)
+    {
+        if(filename[i] == '/')
+        {
+            j = 0;
+            continue;
+        }
+        name[j++] = filename[i];
+    }
+    name[j] = '\0';
+
+    if(lstat(filename, &buf) == -1) 
+    {
+        printf("***%s\n", filename);
+        my_err("stat", __LINE__);
+    }
+    switch (flag_param){
+        case _NONE :
+            if(name[0] != '.') display_1(name, filename);
+            break;
+        case _A :
+            display_1(name, filename);
+            break;
+        case _L :
+            if(name[0] != '.') display_attribute(buf, name, filename);
+            break;
+        case _L+_A :
+            display_attribute(buf, name, filename);
+            break;
+        default :
+            break;
+    }   
+}
+
+void display_dir(int flag_param, char* path)
+{
+    DIR* dir;
+    char temp[MAX] ;
+    struct dirent* ptr;
+    int filesnum = 0;
+    int len = strlen(path);
+
+    dir = opendir(path);
+
+    if(dir == NULL) my_err("opendir",__LINE__);
+
+    while((ptr = readdir(dir)) != NULL)
+    {
+        filesnum ++;
+    }
+    closedir(dir);
+
+    char **filenames = (char**)malloc(sizeof(char*)*filesnum);
+
+    dir = opendir(path);
+    for(int i = 0; i < filesnum; i++)
+    {
+        ptr = readdir(dir);
+        if(ptr == NULL) my_err("readdir",__LINE__);
+        filenames[i] = (char*)malloc(sizeof(char)*MAX);
+        strncpy(filenames[i], path, len);
+        if(filenames[i][len-1] != '/')
+        {
+            filenames[i][len] = '/';
+            filenames[i][len+1] = '\0';
+            strcat(filenames[i],ptr->d_name);
+            filenames[i][len+strlen(ptr->d_name)+1] = '\0';
+        }
+        else 
+        {
+            filenames[i][len] = '\0';
+            strcat(filenames[i], ptr->d_name);
+            filenames[i][len+strlen(ptr->d_name)] = '\0';
+        }
+    }
+
+    quicksort(filenames, 0, filesnum-1);
+
+    if(_R)
+    {
+        printf("\n%s :\n", path);
+    }
+    
+    for(int i = 0; i < filesnum; i++)
+        display(flag_param,filenames[i]);
+    printf("\n");
+    closedir(dir);
+
+    if(_R){
+        for(int i = 2; i < filesnum; i++)
+        {       
+                display_r(flag_param, filenames[i]);
+        }
+    }
+}
+
+void display_1(char* name, char* filename)
+{
+    int i , len;
+    if(g_leave_len < g_maxlen)
+    {
+        printf("\n");
+        g_leave_len = MAXROWLEN;
+    }
+    len = strlen(name);
+    len = g_maxlen - len;
+
+    int color = get_color(filename);
+    printf("\033[%dm%s\033[0m\t", color, name);
+
+    for(i = 0; i < len; i++)
+    {
+        printf("  ");
+        g_leave_len -= (g_maxlen + 2);
+    }
+}
+
+void display_attribute(struct stat buf, char * name, char* filename)
+{
+    char buf_time[32];
     struct passwd* psd;
     struct group* grp;
 
@@ -98,92 +347,94 @@ void my_err(const char* err_string, int line)
 
     printf(" ");
 
-    printf("%4d ", buf.st_nlink);
+    printf("%4ld ", buf.st_nlink);
 
-    psd = getwuid(buf.st_uid);
-    grp = getrgid(buf.st_gid);
-    printf("%-8s", psd->pw_name);
-    printf("%-8s", grp->gr_name);
+    psd = getpwuid(buf.st_uid);
+    grp = getgrgid(buf.st_gid);
 
-    printf("%6d ", buf.st_size);
+    printf("%-6s", psd->pw_name);
 
-    printf("%s ",buf.st_mtime);
-}
-*/
-void display_single()
-{
+    printf("%-6s", grp->gr_name);
 
-}
+    printf("%6ld ", buf.st_size);
 
-char** get_files(const char* path)
-{
-    DIR* dir;
-    struct dirent* ptr;
-    struct dirent* ptrr;
-    int j = 0;
-    
-    if(dir = opendir(path) == NULL)
-    {
-        perror("opendir");
-        exit(1);
-    }
+    strcpy(buf_time, ctime(&buf.st_mtime));
+    buf_time[strlen(ctime(&buf.st_mtime))-1] = '\0';
+    printf("%s  ", buf_time);
 
-    while((ptr = readdir(dir)) != NULL)
-    {
-        files_num ++;
-    }
 
-    char** files = (char**) malloc (sizeof(char*)*files_num);
+    int color = get_color(filename);
+    printf("\033[%dm%s\033[0m", color, name);
 
-    while((ptrr = readdir(dir)) != NULL)
-    {
-        files[j] = (char*) malloc (sizeof(char) * (ptrr->d_reclen));
-        for(int i = 0; i<ptrr->d_reclen; i++)
-        {
-            file_num[i] = ptrr->d_reclen;
-            files[j][i] = ptrr->d_name[i];
-        }
-    }
-
-    return files;
-}
-
-void display_2(char** files)
-{
-    for(int i = 0; i < files_num; i++)
-    {
-        for(int j = 0; j < file_num[i]; j++)
-        {
-            printf("%c",files[i][j]);
-        }
-        printf("  ");
-    }
-}
-
-void display_1(char** files)
-{
-    for(int i = 0; i < files_num; i++)
-    {
-        if(files[i][0] != '.')
-        {   
-            for(int j = 0; j < file_num[i]; j++)
-            {
-                printf("%c",files[i][j]);
-            }
-        printf("  ");
-        }
-    }
-}
-int main(int argc, char** argv)
-{
-    char path[100];
-    char** files;
-    strcpy(path,"./");
-    files = get_files(path);
-    display_1(files);
     printf("\n");
-    printf("\n");
-    display_2(files);
+}
 
-    return 0;
+void my_err(const char* err_string, int line)
+{
+    fprintf(stderr, "line : %d ", line);
+    perror(err_string);
+    exit(1);
+}
+
+int get_color(char* filename)
+{
+    struct stat buf;
+    int color;
+
+    if(lstat(filename, &buf) == -1) my_err("stat", __LINE__);
+
+    if((buf.st_mode & S_IXOTH) || (buf.st_mode & S_IXUSR) || (buf.st_mode & S_IXGRP))
+        color = 32;
+    else if(S_ISLNK(buf.st_mode)){
+        color = 36;
+    }else if(S_ISREG(buf.st_mode)){
+        color = 37;
+    }else if(S_ISDIR(buf.st_mode)){
+        color = 34;
+    }else if(S_ISCHR(buf.st_mode)){
+        color = 33;
+    }else if(S_ISBLK(buf.st_mode)){
+        color = 33;
+    }else if(S_ISFIFO(buf.st_mode)){
+        color = 35;
+    }else if(S_ISSOCK(buf.st_mode)){
+        color = 35;
+    }
+
+    return color;
+}
+
+void quicksort(char **a, int left, int right)
+{
+    if(left < right)
+    {
+        int get = getmid(left, right, a);
+        quicksort(a, left, get-1);
+        quicksort(a, get+1, right);
+    }
+}
+
+int getmid(int left,int right, char **a)
+{
+    char get[MAX];
+    strcpy(get, a[right]);
+    while(left < right)
+    {   
+        while(left<right &&  (strcmp(a[left], get) <= 0))
+            left++;
+        swap(a, right, left);   
+        while(left<right && (strcmp(a[right], get) >= 0))
+            right--;
+        swap(a, left, right);
+    }
+    strcpy(a[left], get);
+    return right;
+}
+
+void swap(char** a, int i, int j)
+{
+    char tem[MAX];
+    strcpy(tem, a[i]);
+    strcpy(a[i], a[j]);
+    strcpy(a[j], tem);
 }
