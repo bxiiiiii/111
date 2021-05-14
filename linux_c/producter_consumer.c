b42f8415dc3799f24232329ad4bd78d72d1236f2
binxxi <1551097460@qq.com>
2021-05-14 10:56:50 +0800

    mpc

--- linux_c/producter_consumer.c
+++ linux_c/producter_consumer.c
@@ -1,74 +1,134 @@
 #include <stdio.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
-//节点结构体
-struct msg
+
+#define MAX 5
+
+typedef struct Node
 {
-    int num; //数据区
-    struct msg *next; //链表区
-};
- 
-struct msg *head = NULL;//头指针
-struct msg *mp = NULL;  //节点指针
-//利用宏定义的方式初始化全局的互斥锁和条件变量
+	int ppp;
+	struct Node* next;
+}node;
+
+int num;
+node *head = NULL;
+node *tail = NULL;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t has_product = PTHREAD_COND_INITIALIZER;
- 
-void *producter(void *arg)
+
+void *producter1()
 {
-    while (1) 
+	while(1)
 	{
-        mp = malloc(sizeof(struct msg));
-        mp->num = rand() % 400 + 1;
-        printf("---producted---%d\n", mp->num);
- 
-        pthread_mutex_lock(&mutex);//访问共享区域必须加锁
-        mp->next = head;
-        head = mp;
-        pthread_mutex_unlock(&mutex);
- 
-        pthread_cond_signal(&has_product);//通知消费者来消费
-		
-        sleep(rand() % 3);
-    }
- 
-    return NULL;
+		if(num < MAX)
+		{
+			pthread_mutex_lock(&mutex);
+
+			tail = (node *)malloc(sizeof(node));
+			tail->ppp = rand() % 400; 
+			tail->next = head;
+			head = tail;
+			num++;
+
+			printf("producter1 : %d\n", tail->ppp);
+
+			pthread_mutex_unlock(&mutex);
+
+			pthread_cond_signal(&cond);
+
+		}
+		else printf("maxmax1\n");
+
+		sleep(rand() % 2);
+	}
 }
- 
-void *consumer(void *arg)
+
+void *producter2()
 {
-    while (1)
+	while(1)
 	{
-        pthread_mutex_lock(&mutex);//访问共享区域必须加锁
-        while (head == NULL)//如果共享区域没有数据，则解锁并等待条件变量
-	{
-            pthread_cond_wait(&has_product, &mutex);
-        }
-        mp = head;
-        head = mp->next;
-        pthread_mutex_unlock(&mutex);
- 
-        printf("------------------consumer--%d\n", mp->num);
-        free(mp); //释放被删除的节点内存
-        mp = NULL;//并将删除的节点指针指向NULL，防止野指针
-		
-        sleep(rand() % 3);
-    }
- 
-    return NULL;
+		if(num < MAX)
+		{
+			pthread_mutex_lock(&mutex);
+
+			tail = (node *)malloc(sizeof(node));
+			tail->ppp = rand() % 400; 
+			tail->next = head;
+			head = tail;
+			num++;
+
+			printf("producter2 : %d\n", tail->ppp);
+
+			pthread_mutex_unlock(&mutex);
+
+			pthread_cond_signal(&cond);
+
+		}
+		else printf("maxmax2\n");
+		sleep(rand() % 2);
+	}
 }
- 
-int main(void)
+
+void *consumer1()
 {
-    pthread_t ptid, ctid;
- 
-    //创建生产者和消费者线程
-    pthread_create(&ptid, NULL, producter, NULL);
-    pthread_create(&ctid, NULL, consumer, NULL);
-    //主线程回收两个子线程
-    pthread_join(ptid, NULL);
-    pthread_join(ctid, NULL);
- 
-    return 0;
+	while(1)
+		{
+			pthread_mutex_lock(&mutex);
+			if(head == NULL)
+				{
+					printf("minmin1\n");
+					pthread_cond_wait(&cond, &mutex);
+				}
+			tail = head;
+			head = tail->next;
+
+			printf("--------------------consumer1 : %d\n", tail->ppp);
+			free(tail);
+			tail = NULL;
+			num--;
+
+			pthread_mutex_unlock(&mutex);
+
+			sleep(rand() % 3);
+		}
+}
+
+void *consumer2()
+{
+	while(1)
+		{
+			pthread_mutex_lock(&mutex);
+			if(head == NULL)
+				{
+					printf("minmin2\n");
+					pthread_cond_wait(&cond, &mutex);
+				}
+			tail = head;
+			head = tail->next;
+
+			printf("-----------------------------------consumer2 : %d\n", tail->ppp);
+			free(tail);
+			tail = NULL;
+			num--;
+
+			pthread_mutex_unlock(&mutex);
+
+			sleep(rand() % 3);
+		}
+}
+
+int main()
+{
+	pthread_t tid1, tid2, tid3, tid4;
+	pthread_create(&tid1, NULL, producter1, NULL);
+	pthread_create(&tid2, NULL, producter2, NULL);
+	pthread_create(&tid3, NULL, consumer1, NULL);
+	pthread_create(&tid4, NULL, consumer2, NULL);
+
+	pthread_join(tid1, NULL);
+	pthread_join(tid2, NULL);
+	pthread_join(tid3, NULL);
+	pthread_join(tid4, NULL);
+	return 0;
 }
