0648458c35f2dbf4d7be5b3e5751847d14023ac3
binxxi <1551097460@qq.com>
2021-05-14 10:57:49 +0800

    pro_se

--- net/pro_server.c
+++ net/pro_server.c
@@ -6,6 +6,16 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <ctype.h>
+#include <signal.h>
+#include <wait.h>
+#include <string.h>
+
+void catch_child(int signum)
+{
+    while((waitpid(0, NULL,WNOHANG))>0);
+
+    return ;
+}
 
 int main()
 {
@@ -14,7 +24,7 @@ int main()
     socklen_t clen = sizeof(caddr);
     pid_t pid;
     int ret;
-    char buf[BUFSIZ];
+    char buf[BUFSIZ], str[INET_ADDRSTRLEN];;
 
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(9999);
@@ -29,6 +39,7 @@ int main()
     while(1)
     {
         cfd = accept(sfd, (struct sockaddr *)&caddr, &clen);
+        printf("connection -- port:%d\tip:%s\n", ntohs(caddr.sin_port), inet_ntop(AF_INET, &caddr.sin_addr.s_addr, str, sizeof(str)));
         pid = fork();
         switch (pid){
             case 0:
@@ -36,18 +47,34 @@ int main()
                 while(1)
                 {
                     ret = read(cfd, buf, sizeof(buf));
-                    for(int i=0; i<ret; i++) 
-                        buf[i] = toupper(buf[i]);
-                    write(cfd, buf, ret);
-                    write(STDOUT_FILENO, buf, ret);
+                    if(ret == 0)
+                    {
+                        printf("connection terminated.\n");
+                        break;
+                    }
+                    else
+                    {
+                        for(int i=0; i<ret; i++) 
+                            buf[i] = toupper(buf[i]);
+                        write(cfd, buf, ret);
+                        write(STDOUT_FILENO, buf, ret);
+                    }
                 }
                 break;
             case -1:
                 perror("fork");
                 exit(1);
             default:
+            {    
+                struct sigaction act;
+                act.sa_handler = catch_child;
+                sigemptyset(&act.sa_mask);
+                act.sa_flags = 0;
+                
+                sigaction(SIGCHLD, &act, NULL);
                 close(cfd);
                 continue;
+            }
         }
     }
     return 0;
