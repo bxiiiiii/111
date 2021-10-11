c336b1996b34e2149c7230610a41e6705b80dcc6
bxiiiiii <1551097460@qq.com>
2021-10-11 22:18:48 +0800

    Integer to English Words

---
+++ c++/Convert number.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+        vector<string> singles = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        vector<string> teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        vector<string> tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    string numberToWords(int num) {
+        if(!num)
+            return "Zero";
+            
+        string str = split(num);
+        str.pop_back();
+
+        return str;
+        
+    }
+    string split(int num)
+    {
+        string str = "";
+        int billion = num / 1000000000;
+        num %= 1000000000;
+        int million = num / 1000000;
+        num %= 1000000;
+        int thousand = num / 1000;
+        num %= 1000;
+    
+        if(billion)
+            str += split(billion) + "Billion ";
+        if(million)
+            str += split(million) + "Million ";
+        if(thousand)
+            str += split(thousand) + "Thousand ";
+
+        str += f(num);
+
+        return str;
+    }
+
+    string f(int n)
+    {
+        string str = "";
+
+        int hundred = n/100;
+        if(hundred)
+            str += singles[hundred] + " Hundred ";
+
+        n %= 100;
+        if(n < 20 && n >= 10){
+            str += teens[n%10] + " ";
+            return str;
+        }else if(n >= 20)
+            str += tens[n/10] + " ";
+        n %= 10;
+        if(!n)
+            return str;
+        
+        str += singles[n%10] + " ";
+
+        return str;
+    }
+};
