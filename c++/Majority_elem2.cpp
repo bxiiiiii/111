0cce64e5cc7740e0f93e58cffd688532fe8a5f52
bxiiiiii <1551097460@qq.com>
2021-10-24 23:03:30 +0800

    update moore vote

--- c++/Majority_elem2.cpp
+++ c++/Majority_elem2.cpp
@@ -26,3 +26,47 @@ public:
         return ans;
     }
 };
+
+class Moore_vote{
+public:
+    vector<int> majorityElement(vector<int>& nums){
+        vector<int> ans;
+        int elem1 = 0;
+        int elem2 = 0;
+        int vote1 = 0;
+        int vote2 = 0;
+
+        for(auto& i : nums){
+            if(vote1 > 0 && i == elem1){
+                vote1++;
+            }else if(vote2 > 0 && i == elem2)
+                vote2++;
+            else if(vote1 == 0){
+                elem1 = i;
+                vote1++;
+            }else if(vote2 == 0){
+                elem2 = i;
+                vote2++;
+            }else{
+                vote1--;
+                vote2--;
+            }
+        }
+
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(auto& i : nums){
+            if(vote1 > 0 && i == elem1)
+                cnt1++;
+            if(vote2 > 0 && i == elem2)
+                cnt2++;
+        }
+
+        if(vote1 > 0 && cnt1 > nums.size() / 3)
+            ans.push_back(elem1);
+        if(vote2 > 0 && cnt2 > nums.size() / 3)
+            ans.push_back(elem2);
+
+        return ans;
+    }
+};
