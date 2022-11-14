0c96e1a8d328f065d874b9487220122b6af7774c
bxiiiiii <1551097460@qq.com>
2022-11-14 21:37:33 +0800

    2p

---
+++ code/lc-160.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+ public:
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    if (headA == nullptr || headB == nullptr) return nullptr;
+    ListNode *pA = headA;
+    ListNode *pB = headB;
+    while (pA != pB) {
+      if (pA == nullptr)
+        pA = headB;
+      else
+        pA = pA->next;
+      if (pB == nullptr)
+        pB = headA;
+      else
+        pB = pB->next;
+    }
+    return pA;
+  }
+};
